576e1a1e3ca9c3a95f996663e192fd70a3daab40
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2015-11-27 16:14:25 +0100

    delted messy files from jhennies

--- orthos_cpp/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jhennies/src/jh_orthos/orthos/orthos_cpp/src
-# Build directory: /home/jhennies/src/jh_orthos/orthos/orthos_cpp/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(python)
